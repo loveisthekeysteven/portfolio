edb18890681c3d0bb5ba2a89b02d89f0b0ea209f
Solomon Chukwuka Emmanuel <solomon.chukwuka28@gmail.com>
2024-04-05 22:49:18 +0100

    more project

--- src/Data.js
+++ src/Data.js
@@ -1,23 +1,42 @@
 export const data = [
   {
-    title: 'The BarberShop',
-    description: ' The website showcases our services, team, and gallery of hairstyles. It incorporates a user-friendly booking system, allowing clients to easily schedule appointments. Additionally, I implemented responsive design techniques to ensure the website looks great on all devices. Explore our website to discover the art of grooming and book your next appointment with us.',
-    projectLink: 'https://barbershop-iota.vercel.app/',
-    codeLink: 'https://github.com/loveisthekeysteven/barbershop',
+    title: "The BarberShop",
+    description: "Unisex Salon",
+    projectLink: "https://barbershop-iota.vercel.app/",
+    codeLink: "https://github.com/loveisthekeysteven/barbershop",
     imgUrl:
-      'https://cdn.sanity.io/images/r8q0vyfe/production/a8a2452252190d05e99ea8a98773da7852c09959-2795x1788.png',
-    tags: ['Web App', 'All'],
-    stacks: ['Html', 'CSS', 'Vanilla JS', 'Git', 'Vercel'],
+      "https://cdn.sanity.io/images/r8q0vyfe/production/a8a2452252190d05e99ea8a98773da7852c09959-2795x1788.png",
+    tags: ["Web App", "All"],
+    stacks: ["Vanilla JS"],
   },
   {
-    title: 'The Weather',
-    description: 'Built a weather application that displays the weather conditions of over 200,000 cities with a change of background depending on the weather condition and time of day of the particular city.',
-    projectLink: 'https://weather-app-khaki-beta.vercel.app/',
-    codeLink: 'https://github.com/loveisthekeysteven/WeatherApp',
+    title: "The Weather",
+    description: "Weather application",
+    projectLink: "https://weather-app-khaki-beta.vercel.app/",
+    codeLink: "https://github.com/loveisthekeysteven/WeatherApp",
     imgUrl:
-      'https://cdn.sanity.io/images/r8q0vyfe/production/1a45d5dc7664c83e311bbb7c687946fbf61243e7-3000x1791.png',
-    tags: ['Web App', 'All'],
-    stacks: ['Html', 'CSS', 'Vanilla JS','WeatherAPI', 'Git', 'Vercel'],
+      "https://cdn.sanity.io/images/r8q0vyfe/production/1a45d5dc7664c83e311bbb7c687946fbf61243e7-3000x1791.png",
+    tags: ["Web App", "All"],
+    stacks: ["Vanilla JS", "WeatherAPI"],
+  },
+  {
+    title: "JustMove Global",
+    description: "Travel Website",
+    projectLink: "https://www.justmoveglobal.com/",
+    codeLink: "https://github.com/loveisthekeysteven/JustMove-Global",
+    imgUrl:
+      "https://cdn.sanity.io/images/r8q0vyfe/production/ec69ecc85d4bb69d6ff869e23ff31a2ade89d419-2972x1740.png",
+    tags: ["Web App", "React JS", "All"],
+    stacks: ["React", "Sass"],
+  },
+  {
+    title: "IMDB",
+    description: "IMDB Clone",
+    projectLink: "https://imdb-clone-theta-sable.vercel.app/",
+    codeLink: "https://github.com/loveisthekeysteven/IMDb",
+    imgUrl:
+      "https://cdn.sanity.io/images/r8q0vyfe/production/4c0bb6ed5548955d32e4c1ce2f1016eb3c6be3b0-3000x1755.png",
+    tags: ["Web App", "React JS", "All"],
+    stacks: ["Nextjs", "Tailwind", "APi"],
   },
 ];
-
