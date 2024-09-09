04a793489fc0d6d5431e0a04bb0a5ef86de67a78
Solomon Chukwuka Emmanuel <solomon.chukwuka28@gmail.com>
2024-09-09 09:40:13 +0100

    new

--- src/Data.js
+++ src/Data.js
@@ -1,15 +1,5 @@
 export const data = [
   {
-    title: "Natours",
-    description: "Tourist Booking Application",
-    projectLink: "https://natours-jmtc.onrender.com/",
-    codeLink: "https://github.com/loveisthekeysteven/Natours",
-    imgUrl:
-      "https://cdn.sanity.io/images/r8q0vyfe/production/2bc8d0605c878e03b19694ee3cd02225fd1f3495-2917x1729.png",
-    tags: ["Web App", "All"],
-    stacks: ["Express.Js", "MongodDB", "Pug", "Stripe"],
-  },
-  {
     title: "JustMove Global",
     description: "Travel Website",
     projectLink: "https://www.justmoveglobal.com/",
@@ -18,6 +8,26 @@ export const data = [
       "https://cdn.sanity.io/images/r8q0vyfe/production/ec69ecc85d4bb69d6ff869e23ff31a2ade89d419-2972x1740.png",
     tags: ["React JS", "All"],
     stacks: ["React", "Sass"],
+  },
+  {
+    title: "ZionFoodSmart",
+    description: "Grocery Store",
+    projectLink: "https://zionfoodsmart.com/",
+    codeLink: "https://github.com/loveisthekeysteven/",
+    imgUrl:
+      "https://cdn.sanity.io/images/r8q0vyfe/production/16b58e58ca46d71465d790738bca42d39e341bc9-2953x1716.png",
+    tags: ["All"],
+    stacks: ["WordPress", "WooCommerce"],
+  },
+  {
+    title: "Natours",
+    description: "Tourist Booking Application",
+    projectLink: "https://natours-jmtc.onrender.com/",
+    codeLink: "https://github.com/loveisthekeysteven/Natours",
+    imgUrl:
+      "https://cdn.sanity.io/images/r8q0vyfe/production/2bc8d0605c878e03b19694ee3cd02225fd1f3495-2917x1729.png",
+    tags: ["Web App", "All"],
+    stacks: ["Express.Js", "MongodDB", "Pug", "Stripe"],
   },
   {
     title: "IMDB",
